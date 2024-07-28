66760a530d1cde0b59cfd3ea314c934bd7a8acc1
SirFlyingF <kabirsharma2905@gmail.com>
2024-07-28 21:36:48 +0530

    Allowed all hosts in Django layer

--- testmaker/testmaker/settings.py
+++ testmaker/testmaker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yfwj+dw*g6v(k05b5slvmw)n_e+$_=z&myw3x72iyj+d3#0ah_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
