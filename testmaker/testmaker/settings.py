8eb72c3bc376619a10fe0defbe8e7aca67dda122
SirFlyingF <kabirsharma2905@gmail.com>
2024-08-25 14:23:26 +0530

    Database connection to Postgres

--- testmaker/testmaker/settings.py
+++ testmaker/testmaker/settings.py
@@ -141,6 +141,18 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
+# DB settings
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
+    }
+}
+
 # env settings
 JWT_SECRET = os.getenv('JWT_SECRET')
 DOMAIN = os.getenv('DOMAIN')
