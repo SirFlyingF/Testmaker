d425bcfd72b8e9f2d0adc16baa179feab49c513f
SirFlyingF <kabirsharma2905@gmail.com>
2025-01-18 23:49:17 +0530

    Dangling S3 file handling

--- Django/common/models.py
+++ Django/common/models.py
@@ -194,10 +194,10 @@ def generate_thumbnail(sender, instance, **kwargs):
                 return
             else:
                 # Delete files from S3 if it has been changed
-                if instance.file:
-                    instance.file.delete(save=False)
-                if instance.thumbnail:
-                    instance.thumbnail.delete(save=False)   
+                if old_instance.file:
+                    old_instance.file.delete(save=False)
+                if old_instance.thumbnail:
+                    old_instance.thumbnail.delete(save=False)   
 
         fname, ftype = os.path.splitext(instance.file.name)
         if not ftype.lower() == '.pdf':
